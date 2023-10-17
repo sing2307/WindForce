e40c7f2797abb8e0ea80230e3c126ec0a1715d77
Elias <eliasperras@outlook.de>
2023-10-17 12:21:01 +0200

    Init definition of ABC

---
+++ calculation.py
@@ -0,0 +1,62 @@
+from typing import Dict
+from abccalculation import ABCCalculation
+
+
+class Calculation(ABCCalculation):
+    """
+    Concrete class for calculation
+    """
+
+    def __init__(self, sections):
+        """
+        ...
+        :param element_parameters:
+        """
+        super().__init__(sections)
+
+    def return_solution(self):
+        """
+        ...
+        :return:
+        """
+        print("kek")
+        #return NotImplemented
+
+    def assembly_system_matrix(self):
+        """
+        Calculates element matrices for every section, assembly system
+        :return:
+        """
+        return NotImplemented
+
+    def solve_system(self):
+        """
+        Solves for eigenfrequencies and the respective nodes displacement
+        :return:
+        """
+        return NotImplemented
+
+class Elements():
+    """
+    Computation of system matrices and solution
+    """
+
+    def __init__(self, element_parameters):
+        """
+        ...
+        :param element_parameters:
+        """
+        self.element_parameters = element_parameters
+
+    def element_matrix(self):
+        """
+        Calculates element matrix
+        :return:
+        """
+        return NotImplementedError
+
+
+element_parameters = {1: {'length': 1, 'diameter': 0.5},
+                      2: {'length': 2, 'diameter': 0.25}}
+calc = Calculation(element_parameters)
+
